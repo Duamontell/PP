ed86e916550644c3143b99306d4153b7496dbeaf
Maksim <duamontell@gmail.com>
2025-11-08 21:05:20 +0300

    feat: Сделал 2 и 3 задания в 6 лабе

---
+++ lab6/task_2_main.cpp
@@ -0,0 +1,19 @@
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <omp.h>
+#include <valarray>
+
+int main()
+{
+	int x = 44;
+	// #pragma omp parallel for private(x)
+	// #pragma omp parallel for firstprivate(x)
+	#pragma omp parallel for lastprivate(x)
+	for(int i = 0; i <= 10; i++){
+		// x=i;
+		x=i;
+		printf("Thread number: %d x: %d\n",omp_get_thread_num(),x);
+	}
+	printf("x is %d\n", x);
+}
