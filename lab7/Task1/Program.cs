806599f05fc05dee08dc2ad1418a44cdae8f7f32
Maksim <duamontell@gmail.com>
2025-11-14 21:46:23 +0300

    feat: Первое задание без асинхронного

---
+++ lab7/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task1;
+
+public static class Program
+{
+    public static void Main()
+    {
+        string fileName = "";
+        do
+        {
+            Console.Write("Введите имя файла: ");
+            string? input = Console.ReadLine();
+            if (input != null)
+            {
+                fileName = input.Trim();
+            }
+        } while (string.IsNullOrWhiteSpace(fileName) || int.TryParse(fileName, out _));
+
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("Ошибка, данного файла не существует!");
+            return;
+        }
+
+        Console.Write("Введите символы для удаления: ");
+        string? symbols = Console.ReadLine();
+        
+        if (string.IsNullOrEmpty(symbols))
+        {
+            Console.WriteLine("Символы для удаления не заданы. Файл не изменён.");
+            return;
+        }
+        
+        string[] lines = File.ReadAllLines(fileName);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string result = lines[i];
+            foreach (char symbol in symbols)
+            {
+                result = result.Replace(symbol.ToString(), "");
+            }
+            
+            lines[i] = result;
+            Console.WriteLine(result);
+        }
+        
+        File.WriteAllLines(fileName, lines);
+    }
+}
